2e3ee8e82c10897bd3f6f5ae8f10791f7f93627f
Chetan <43693378+chetangaur@users.noreply.github.com>
2019-06-19 14:16:52 +0530

    Solution2
    Basics of numpy array

---
+++ Problem2.py
@@ -0,0 +1,11 @@
+    
+# if we make matrix without difference of 5
+import numpy as np
+z= np.random.randint(100,200,size=(8,2))
+z
+
+# to make matrix with difference of 5 but in range of 100 to 180
+ q=np.arange(100,180,5)
+
+r=q.reshape(8,2)
+r
